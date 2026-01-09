79e655a1c36aff53d4d601fc0078fef0e2f1b769
Bhavisha Panchal <145912697+Bhavisha-16@users.noreply.github.com>
2026-01-09 11:07:45 +0530

    code

--- backend/config.py
+++ backend/config.py
@@ -4,5 +4,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-SUPABASE_URL = os.getenv("https://bvfaxqyetripdntedhuq.supabase.co")
-SUPABASE_KEY = os.getenv("sb_publishable_t8buKJZFe5lxeXTPh6kNCw_MCEtaJfW")
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
