0fc51ff4ac0e5be95ae48b61bd3786e38c920cbf
rahuldev-singh <143645420+rahuldev-singh@users.noreply.github.com>
2026-01-09 14:59:33 +0530

    Add API Key

--- frontend/js/supabaseClient.js
+++ frontend/js/supabaseClient.js
@@ -1,7 +1,7 @@
 // DO NOT redeclare `supabase`
 
 const SUPABASE_URL = "https://bvfaxqyetripdntedhuq.supabase.co";
-const SUPABASE_ANON_KEY = "PASTE_YOUR_REAL_ANON_PUBLIC_KEY_HERE";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ2ZmF4cXlldHJpcGRudGVkaHVxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc4Njk3MDQsImV4cCI6MjA4MzQ0NTcwNH0.ZMds4tWytEUusAP0GycF6LI4UfxBZ18QWsi6KxZkAA8";
 
 // create client using CDN global
 window.supabaseClient = window.supabase.createClient(
